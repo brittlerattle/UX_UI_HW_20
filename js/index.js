b4c7d6bdd38419339b61b5955fb57b9924bfc90e
brittlerattle <andrew.d.bell@gmail.com>
2021-12-03 01:54:41 -0700

    animation change

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ console.log("Your index.js file is loaded corretly!");
 $(document).ready(function(){
     $(".bubble").hover(
         function() {$(this).attr("src","images/splash images/bubble2.png");},
-        function() {$(this).attr("src","images/splash images/bubble.png");
-  
+        function() {$(this).attr("src","images/splash images/bubble.png");},
+        function() {
+            $(this).effect( "shake" );
+          });
     });
-});
-
