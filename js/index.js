f50bbfc464c56f9e4ae777a70cfbc1ea060d5f58
brittlerattle <andrew.d.bell@gmail.com>
2021-12-03 14:49:56 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
     $(document).ready(function(){
         $(".bubble").hover(
-            function() {$(this).animate( {left: '25vw', bottom: '32vh'}, 1000 );
+            function() {$(this).animate( {left: '22vw', bottom: '31vh'}, 1000 );
         });
         $(".bubble").mouseleave(
             function() {$(this).animate( {left: '20vw', bottom: '30vh'}, 100 );
