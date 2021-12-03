a39370ef98e7474ca2f25f1ca729739ba52d03b6
brittlerattle <andrew.d.bell@gmail.com>
2021-12-03 11:46:57 -0700

    more javascript animations

--- js/index.js
+++ js/index.js
@@ -4,8 +4,16 @@ console.log("Your index.js file is loaded corretly!");
 $(document).ready(function(){
     $(".bubble").hover(
         function() {$(this).attr("src","images/splash images/bubble2.png");},
-        function() {$(this).attr("src","images/splash images/bubble.png");},
-        function() {
-            $(this).effect( "shake" );
+        function() {$(this).attr("src","images/splash images/bubble.png");
           });
     });
+
+
+    $(document).ready(function(){
+        $(".bubble").hover(
+            function() {$(this).animate( {left: '25vw', bottom: '32vh'}, 1000 );
+        });
+        $(".bubble").mouseleave(
+            function() {$(this).animate( {left: '20vw', bottom: '30vh'}, 100 );
+        });
+    })
